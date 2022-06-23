1124c083b43a9ba9982c4018488646425796a191
vladlendv <53654603+vladlendv@users.noreply.github.com>
2022-06-23 17:07:43 +0500

    add lazyload for gallery page

---
+++ lazyLoad.js
@@ -0,0 +1,22 @@
+let images = document.querySelectorAll('.gallerys .gallery-card');
+
+const lazyLoad = target => {
+        const io = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    let div = entry.target;
+                    let img = entry.target.childNodes[1].childNodes[1];
+                    let src = img.getAttribute('data-src');
+                    img.setAttribute('src', src);
+                    div.classList.add('fade-img');
+                  	div.classList.remove('lazy-img');
+    
+                    observer.disconnect();
+                }
+            })
+        })
+    
+        io.observe(target);
+    }
+
+images.forEach(lazyLoad);
